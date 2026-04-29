e65b98d698833ac4f27f341e8278484d639c8903
Aman <amanpatel17122003@gmail.com>
2026-04-29 21:57:35 +0530

    Loop in Javascript

---
+++ Assignment/Javascript/JSLoop/script.js
@@ -0,0 +1,34 @@
+// for(var i=0; i<=10;i++)
+// {
+//     console.log(i)
+// }
+
+// let a=[24,58,33,86,96,12,53,23,32,100]
+// for(let i=0; i<10; i++)
+// {
+//     console.log(a[i])
+// }
+for(let i=1;i<=10; i++)
+{
+    console.log("2*"+i+"="+2*i);
+}
+// let i=0;
+// while(i<10)
+// {
+//     console.log(a[i]);
+//     i++;
+// }
+// let i=1;
+// do{
+//     console.log(i*2);
+//     i++;
+    
+// }while(i<=10);
+
+let a=1;
+while(i<=10)
+{
+    console.log(i*2);
+    a++;
+
+}
