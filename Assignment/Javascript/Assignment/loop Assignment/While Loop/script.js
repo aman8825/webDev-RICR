47644ee075f684d987bf8bbf3cacde0c20d5be9d
Aman <amanpatel17122003@gmail.com>
2026-04-30 16:57:14 +0530

    While loop in javascript

---
+++ Assignment/Javascript/Assignment/loop Assignment/While Loop/script.js
@@ -0,0 +1,116 @@
+// Question1
+let i=1;
+while(i<=5)
+{
+    console.log(i);
+    i++;
+}
+
+
+// Question2
+let j=5;
+while(j>=1)
+{
+    console.log(j);
+    j--;
+}
+
+
+// Question3
+let k=2;
+while(k<11)
+{
+    console.log(k);
+    k+=2;
+}
+
+
+// Question4
+let x=10;
+let sum=0;
+
+while(x>=5)
+{
+    sum=sum+x;
+    x--;
+}
+console.log("Sum =",sum);
+
+
+// Question5
+// initialization = let a=1
+// condition = a<=5
+// increment = a++
+
+let a=1;
+while(a<=5)
+{
+    console.log(a);
+    a++;
+}
+
+
+// Question6
+let b=1;
+while(b<=3)
+{
+    console.log("Step:",b);
+    b++;
+}
+
+
+// Question7
+let c=10;
+while(c<5)
+{
+    console.log(c);
+    c++;
+}
+// condition false at beginning → loop will not run
+
+
+// Question8
+let d=1;
+while(d<=5)
+{
+    console.log(d);
+    // d++ missing → infinite loop
+
+
+// Question9
+let e=1;
+while(e<=5)
+{
+    console.log(e);
+    // e++ missing → infinite loop happens
+}
+
+
+// Question10
+let f=1;
+while(f<=5)
+{
+    console.log(f);
+    f++;
+}
+// condition controls stopping when f>5
+
+
+// Question11
+let g=5;
+while(g>=1)
+{
+    console.log(g);
+    g--;
+}
+// decrement makes numbers decrease
+
+
+// Question12
+let h=1;
+while(h<=4)
+{
+    console.log(h);
+    h++;
+}
+// loop runs 4 times
