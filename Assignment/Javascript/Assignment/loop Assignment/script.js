c000ca99c90e1d3be8ae3c0f7725167a72cfb14a
Aman <amanpatel17122003@gmail.com>
2026-04-30 16:56:54 +0530

    Javascript Loop Assignment

---
+++ Assignment/Javascript/Assignment/loop Assignment/script.js
@@ -0,0 +1,89 @@
+// Question1
+for(let i=1;i<=5;i++)
+{
+    console.log(i);
+
+}
+
+// Question2
+for(let i=1;i<=10;i++)
+{
+    console.log(i);
+
+}
+// Question3
+for(let i=10;i>0;i--)
+{
+    console.log(i);
+
+}
+
+// Question4
+for(let i=2;i<=10;i++)
+{
+    if(i%2==0)
+    {
+console.log(i);
+    }
+    
+
+}
+// Question5
+for(let i=2;i<=10;i++)
+{
+    if(i%2!=0)
+    {
+console.log(i);
+    }
+    
+
+}
+
+// Question6
+for(let i=10;i>0;i--)
+{
+    console.log("15*"+i+"="+15*i);
+
+}
+
+// Question7
+for(let i=1;i<=20;i+=2)
+{
+    console.log(i);
+
+}
+
+
+// Question8
+for(let i=5;i<=50;i+=5)
+{
+    console.log(i);
+
+}
+
+// Question9
+for(let i=1;i<=10;i++)
+{
+    console.log(i*i);
+
+}
+
+// Question10
+for(let i=1;i<=10;i++)
+{
+    if(i%3==0)
+    {
+console.log(i);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
