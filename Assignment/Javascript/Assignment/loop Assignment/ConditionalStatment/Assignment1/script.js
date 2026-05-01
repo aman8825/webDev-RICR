91fe831da8dd421073713fbfeccb1d995464425e
Aman <amanpatel17122003@gmail.com>
2026-05-01 22:49:16 +0530

    Conditional statment assignment1

---
+++ Assignment/Javascript/Assignment/loop Assignment/ConditionalStatment/Assignment1/script.js
@@ -0,0 +1,14 @@
+let num = 20;
+num > 0 ? console.log("true") : console.log("false");
+let x = 6;
+x % 2 == 0 ? console.log("even") : console.log("odd");
+let age = 29;
+age >= 18
+  ? console.log("Eligible fo votting")
+  : console.log("Not Eligible fo votting");
+let signal = "green";
+signal == "green" ? console.log("Go") : console.log("Stop");
+let a=3;
+a>0?console.log("Positive"):console.log("Negetive")
+let number=46;
+number%2==0 ?console.log("divisible by 2"):console.log("Not divisible by 2");
