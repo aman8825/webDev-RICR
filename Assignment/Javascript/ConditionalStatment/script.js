655f29ed921e11a7adc3188ad3f982f0fd1ba45a
Aman <amanpatel17122003@gmail.com>
2026-05-01 22:11:07 +0530

    Conditional Statment in Javascript

---
+++ Assignment/Javascript/ConditionalStatment/script.js
@@ -0,0 +1,91 @@
+// let n = 5;
+// if (n > 0) {
+//   console.log("positive integer");
+// } else {
+//   console.log("Negative integer");
+// }
+
+// let age = 20;
+// if (age >= 18) {
+//   console.log("Eligible to vote");
+// } else {
+//   console.log("Not Eligible to vote");
+// }
+// let num = 10;
+// if (num % 2 == 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+// let marks = 30;
+// if(marks >= 90) {
+//   console.log("A");
+// } else if (marks >= 75)
+// {
+//   console.log("B");
+// }
+// else if(marks >= 50)
+// {
+//   console.log("c");
+// }
+// else
+// {
+//   console.log("fail");
+// }
+
+// let user = "abcde";
+// let pass = "12345";
+
+// if (user === "abcde" && pass === "12345") {
+//   console.log("login succss");
+// } else {
+//   console.log("login faild");
+// }
+// //Short Hand
+// (user === "abcde" && pass === "12345")
+// ? console.log("login success")
+// : console.log("login failed");
+// let data;
+// let N = "Aman";
+// //Short Hand
+// data = N || "N/A";
+// // if(N){
+// //     data=Name;
+
+// // }else{
+// //     data="N/A"
+// // }
+// console.log(data);
+
+// for (let i = 0; i < 5; i++) {
+//   if (i === 3) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+// for (let i = 0; i < 5; i++) {
+//   if (i === 3) {
+//     break;
+//   }
+//   console.log(i);
+// }
+// let choice = 2;
+// switch (choice) {
+//   case 1: {
+//     console.log("Tea");
+//     break;
+//   }
+//   case 2: {
+//     console.log("coffee");
+//     break;
+//   }
+//   case 3: {
+//     console.log("Lassi");
+//     break;
+//   }
+//   default: {
+//     console.log("panni");
+//   }
+// }
+let x=-20;
+(x>0)? console.log("true") :console.log("false");
