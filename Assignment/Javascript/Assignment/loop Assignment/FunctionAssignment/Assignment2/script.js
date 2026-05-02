dc203370a15f7e3ef65b5825f0041dab5bf99df6
Aman <amanpatel17122003@gmail.com>
2026-05-02 15:58:22 +0530

    Function Expressions – Practice Assignment2

---
+++ Assignment/Javascript/Assignment/loop Assignment/FunctionAssignment/Assignment2/script.js
@@ -0,0 +1,27 @@
+let abc =function()
+{
+    console.log("Hello");
+}
+console.log(abc)
+let abcd =function()
+{
+    console.log("Hello");
+}
+abcd();
+let display=function(msg)
+{
+    console.log(msg);
+}
+display("Hello World");
+let displayTwoNumber=function (a,b)
+{
+    console.log(a,b);
+}
+displayTwoNumber(20,40);
+// abcde(30);
+let abcde=function(n)
+{
+    console.log(n);
+}
+abcde(40);
+
