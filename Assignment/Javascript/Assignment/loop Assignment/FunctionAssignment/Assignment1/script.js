d6f34b064d8811816f1762476e58af8ba53e6fdd
Aman <amanpatel17122003@gmail.com>
2026-05-02 14:59:12 +0530

    JavaScript Functions – Practice Assignment1

---
+++ Assignment/Javascript/Assignment/loop Assignment/FunctionAssignment/Assignment1/script.js
@@ -0,0 +1,47 @@
+function print() {
+  console.log("Hello World");
+}
+print();
+function YourName(name) {
+  console.log(name);
+}
+YourName("Aman Kumar");
+function print() {
+  console.log("Welcome to College");
+}
+print();
+function YourName(name) {
+  console.log("Hello " + name);
+}
+YourName("Aman Kumar");
+function sum(a, b) {
+  console.log(a + b);
+}
+sum(10, 20);
+function squar(a) {
+  console.log(a * a);
+}
+squar(10);
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(10, 29));
+function mul(a, b) {
+  return a * b;
+}
+console.log(mul(10, 29));
+function nameAge(name, age) {
+  console.log(name, age);
+}
+nameAge("Aman Kumar", 20);
+
+function average(a,b,c)
+{
+    return (a+b+c)/3;
+}
+console.log(average(1,2,3));
+function greaterNumber(a,b)
+{
+    a>b?console.log(a):console.log(b);
+}
+greaterNumber(28,68);
