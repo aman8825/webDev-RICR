5a9891d659f5bc5462c99b60ca1964bdeff5f494
Aman <amanpatel17122003@gmail.com>
2026-05-07 18:32:49 +0530

    Add HTML and JavaScript files for forEach() assignment

---
+++ Javascript/Assignment/arrayTransformation&Itteration/Assignment1/script.js
@@ -0,0 +1,19 @@
+let data=[2,4,6,1,5,6,7];
+data.forEach((item)=>{
+console.log(item);
+}
+);
+let fruits=["Apple","Banana","Guava","Mango"];
+fruits.forEach((i)=>{
+    console.log(i);
+})
+fruits.forEach((element,idx)=>{
+    console.log(element,idx);
+})
+fruits.forEach((a)=>{
+    console.log(fruits[0]);
+})
+let arr=[3,4,6,8,77,87,54];
+arr.forEach((i)=>{
+    console.log(i+8);
+})
