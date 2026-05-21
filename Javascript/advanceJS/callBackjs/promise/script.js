991d1ab4065e1ec530b8ab9308416eca118812f3
Aman <amanpatel17122003@gmail.com>
2026-05-21 23:56:47 +0530

    Refactor promise example: update HTML structure and enhance script functionality with async/await

--- Javascript/advanceJS/callBackjs/promise/script.js
+++ Javascript/advanceJS/callBackjs/promise/script.js
@@ -22,23 +22,51 @@
 //   .catch((rej) => console.log(rej));
 
 // const myPromise=getData(1);
+// async function getData(value) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if(value===3){
+//             reject("Query Not Found");
+//         }
+//         else{
+// console.log("Data Sent", value);
+//       resolve("Query Solve");
+//         }
+      
+//       // reject("Query Rejected")
+//     }, 5000);
+//   });
+// }
+// await getData(1);
+// await getData(2);
+// await getData(3);
+// await getData(4);
+// await getData(5);
+
 async function getData(value) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-        if(value===3){
-            reject("Query Not Found");
-        }
-        else{
-console.log("Data Sent", value);
-      resolve("Query Solve");
-        }
-      
-      // reject("Query Rejected")
-    }, 5000);
+      if (value === 3) {
+        reject("Query Not Found");
+      } else {
+        console.log("Data Sent", value);
+        resolve("Query Solved");
+      }
+    }, 2000);
   });
 }
-await getData(1);
-await getData(2);
-await getData(3);
-await getData(4);
-await getData(5);
+
+// getData(1)
+//   .then(() => getData(2))
+//   .then(() => getData(3))
+//   .then(() => getData(4))
+//   .then(() => getData(5))
+//   .catch((rej) => console.log(rej));
+
+
+
+await getData(1)
+await getData(2)
+await getData(3)
+await getData(4)
+await getData(5)
