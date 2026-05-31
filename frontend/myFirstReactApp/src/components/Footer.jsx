0fa92013fa49fd85d5968153817dc641e039796f
Aman <amanpatel17122003@gmail.com>
2026-05-31 10:50:36 +0530

    feat: add Bootstrap integration, create Header and Footer components, and remove unused App.css

---
+++ frontend/myFirstReactApp/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <>
+      <div id="footer">This is footer</div>
+    </>
+  );
+}
+export default Footer;
---
+++ frontend/myFirstReactApp/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <>
+      <div id="header">This is header</div>
+    </>
+  );
+}
+export default Header;
